c50a86fd6520e551da866d0eeb3e88717608935e
Ying-Chun Lin <thechunlin@gmail.com>
2022-07-20 16:39:40 +0800

    feat: Detect User’s Location

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,9 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+  background-color: #135058;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/PiniaWeather.vue
+++ src/components/PiniaWeather.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="weather">{{ header }}</div>
+  <div class="weather">
+    <h1>{{ header }}</h1>
+    <div class="row input-row">
+      <button
+        class="btn btn-primary"
+        type="submit"
+        @click="locatorButtonPressed"
+      >
+        Button
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,8 +20,28 @@ export default {
     return {
       header: 'Weather Forecast'
     }
+  },
+  methods: {
+    locatorButtonPressed() {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          console.log(position.coords.latitude)
+          console.log(position.coords.longitude)
+        },
+        (error) => {
+          console.log(error.message)
+        }
+      )
+    }
   }
 }
 </script>
 
-<style></style>
+<style lang="scss">
+.weather {
+  color: #f1f2b5;
+}
+.btn-primary {
+  width: min-content;
+}
+</style>
