62bfd7bf8c520ab21f7eb3dcda02d3f7fa5b99e6
Ying-Chun Lin <thechunlin@gmail.com>
2022-07-22 23:50:45 +0800

    feat: 顯示使用者位置

--- src/components/PiniaWeather.vue
+++ src/components/PiniaWeather.vue
@@ -6,6 +6,8 @@
         <span class="input-group-text" id="basic-addon1">city</span>
         <input type="text" class="form-control" placeholder="cityname" />
       </div>
+      <p>{{ location.name }}</p>
+      <p>{{ location.country }}</p>
     </div>
   </div>
 </template>
@@ -16,32 +18,40 @@ export default {
   data() {
     return {
       header: 'Weather Forecast',
+      url_base:
+        'https://api.weatherapi.com/v1/history.json?key=424993aae23147a1afb32605222207&',
       location: {
         lat: '',
-        lon: ''
-      }
+        lon: '',
+        name: '',
+        country: ''
+      },
+      today: ''
     }
   },
   created() {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.location.lat = position.coords.latitude
-        this.location.lon = position.coords.longitude
-        console.log(this.location.lat)
-        console.log(this.location.lon)
-      },
-      (error) => {
-        console.log(error.message)
-      }
-    )
+    this.today = new Date().toISOString().slice(0, 10)
   },
+  async mounted() {
+    const position = await this.getCoordinates()
+    this.location.lat = position.coords.latitude
+    this.location.lon = position.coords.longitude
 
-  mounted() {
     fetch(
-      'https://api.weatherapi.com/v1/history.json?key=424993aae23147a1afb32605222207&q= 48.8567,2.3508&dt=2022-07-22'
-    ).then((res) => {
-      console.log(res.json())
-    })
+      `${this.url_base}q=${this.location.lat},${this.location.lon}&dt=${this.today}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        this.location.name = data.location.name
+        this.location.country = data.location.country
+      })
+  },
+  methods: {
+    getCoordinates() {
+      return new Promise(function (resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+      })
+    }
   }
 }
 </script>
