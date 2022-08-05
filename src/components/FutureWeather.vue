f3207f33de4445d175f2db153664e03d9acd8f0f
Ying-Chun Lin <thechunlin@gmail.com>
2022-08-05 16:53:04 +0800

    未來六天欄位與日期建立

--- src/components/FutureWeather.vue
+++ src/components/FutureWeather.vue
@@ -8,17 +8,54 @@
         <div class="col-4" style="color: #56719a">
           <h5>{{ apiData.location.country }} {{ apiData.location.name }}</h5>
         </div>
-
         <div class="col-12 p-2">
           <div class="main">
-            <p class="m-3">
-              {{ tomorrowData.forecast.forecastday[0].hour[6].condition.text }}
-            </p>
+            <div class="row">
+              <div class="col-6 align-self-center">
+                <img
+                  :src="
+                    apiData.current.condition.icon.replace('64x64', '128x128')
+                  "
+                  style="align-self: center"
+                />
+                <p class="m-0">
+                  {{ apiData.current.condition.text }}
+                </p>
+              </div>
+              <div class="col-6 align-self-center">
+                <h3 class="m-0">
+                  <weather-pouring-icon fillColor="#3a4e72" :size="48" />
+                  {{
+                    historyApiData.forecast.forecastday[0].hour[hour]
+                      .chance_of_rain
+                  }}%
+                </h3>
+                <h2>
+                  {{ apiData.current.temp_c }}°C/{{ apiData.current.temp_f }}°F
+                </h2>
+                <p class="m-0">
+                  Feellike : {{ apiData.current.feelslike_c }}°C/{{
+                    apiData.current.feelslike_f
+                  }}°F
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
       <div class="row">
-        <div class="col-12 p-2"><div class="secondary">7</div></div>
+        <div class="col-12 p-2">
+          <div class="secondary">
+            <div class="row">
+              <div class="col-2">{{ thirdDay }}</div>
+              <div class="col-2">{{ fourthDay }}</div>
+              <div class="col-2">{{ fifthDay }}</div>
+              <div class="col-2">{{ sixthDay }}</div>
+              <div class="col-2">{{ seventhDay }}</div>
+              <div class="col-2">{{ eigthDay }}</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -27,6 +64,10 @@
 <script>
 import { mapState } from 'pinia'
 import { useUserStore } from '@/store/user'
+
+import moment from 'moment'
+
+import WeatherPouringIcon from 'vue-material-design-icons/WeatherPouring.vue'
 export default {
   computed: {
     ...mapState(useUserStore, [
@@ -42,10 +83,28 @@ export default {
       'tomorrowData'
     ])
   },
+  components: {
+    WeatherPouringIcon
+  },
   data() {
     return {
-      header: 'Weather Forecast'
+      header: 'Weather Forecast',
+      thirdDay: '',
+      fourthDay: '',
+      fifthDay: '',
+      sixthDay: '',
+      seventhDay: '',
+      eigthDay: ''
     }
+  },
+
+  async mounted() {
+    this.thirdDay = moment().add(2, 'days').format('YYYY-MM-DD')
+    this.fourthDay = moment().add(3, 'days').format('YYYY-MM-DD')
+    this.fifthDay = moment().add(4, 'days').format('YYYY-MM-DD')
+    this.sixthDay = moment().add(5, 'days').format('YYYY-MM-DD')
+    this.seventhDay = moment().add(6, 'days').format('YYYY-MM-DD')
+    this.eigthDay = moment().add(7, 'days').format('YYYY-MM-DD')
   }
 }
 </script>
